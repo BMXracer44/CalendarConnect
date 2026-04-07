a35dabf3edf147edbb9f7718fc93d32a521e49d3
Connor Fry <connorfry089@gmail.com>
2026-04-07 14:40:23 -0500

    Update UserController.java

---
+++ backend/src/main/resources/static/controllers/UserController.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody User user){
+        userService.saveUser(user);
+        return "New User is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<User> list(){
+        return userService.getAllUsers();
+    }
+}
