c58d51666900af3874ca97fd2aeefbc4febfa0fe
BMXracer44 <fergusalex444@gmail.com>
2026-04-13 15:11:55 -0500

    Added event repo

---
+++ backend/src/main/java/com/calendarconnect/backend/repository/EventRepository.java
@@ -0,0 +1,31 @@
+package com.calendarconnect.backend.repository;
+
+import com.calendarconnect.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+  /**
+   * Find an event by event_id
+   */
+  Optional<Event> findByEventId(String event_id);
+
+  /**
+   * Find an event by start_datetime
+   */
+  Optional<Event> findByStartDateTime(String start_datetime);
+
+  /**
+   * Check if event_id exists
+   */
+  boolean existsByEventId(String event_id);
+
+  /**
+   * Check if start_datetime exists
+   */
+  boolean existsByStartDateTime(String start_datetime);
+}
