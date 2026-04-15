54b856dbf754e5cac8863ded39adcd236e122048
Brittany S <brittanysanford5@gmail.com>
2026-04-15 19:18:36 +0000

    Fixed CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
